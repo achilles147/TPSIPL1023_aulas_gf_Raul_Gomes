77cd50f289230cdc491cf5760fcc789dd2daa514
achilles147 <raul19david@gmail.com>
2023-11-16 00:33:04 +0000

    exercicio9.listas adicionado, ficheiro texto removido

--- 0789/exercicios/exercicios_listas/ex9.py
+++ 0789/exercicios/exercicios_listas/ex9.py
@@ -3,3 +3,12 @@ Faça um Programa que leia um vetor A com 10 números inteiros,
 calcule e mostre a soma dos quadrados dos elementos do vetor.
 """
 
+vetA = []
+soma = 0
+for i in range(10):
+    num = int(input(f"{i}. Insira um número: "))
+    vect.append(num)
+
+for i in range(len(vetA)):
+    soma += vetA[i]**2
+print(f"A soma dos quadrados dos elementos do vetor é de {soma}")
