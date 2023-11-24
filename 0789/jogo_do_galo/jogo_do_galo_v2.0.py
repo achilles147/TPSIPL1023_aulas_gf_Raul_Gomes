bbed8d08c9c5baf1a6bb6c5e89af6ec6a08f0a97
achilles147 <raul19david@gmail.com>
2023-11-24 15:09:01 +0000

    update

--- 0789/jogo_do_galo/jogo_do_galo_v2.0.py
+++ 0789/jogo_do_galo/jogo_do_galo_v2.0.py
@@ -1,18 +1,19 @@
 
-def makeboard():
+def makeboard(): #cria a board
     n_board = int(input("N-elementos: "))
     for i in range(n_board):
         linha = []
         for j in range(n_board):
             linha.append('_')
         board.append(linha)
-def showboard():
+def showboard(): #print da board
+    print(f"\nRonda {ronda:.0f}\nJogador {jogadores[jogador]}\n"+"-"*20)
     for i in range(len(board)):
         for j in range(len(board)):
             print(board[i][j], end="\t\t")
         print("\n")
 
-def validacao_input(var):
+def validacao_input(var): #valida a input caso não seja int e > ou < que o tamanho da board
     while True:
         try:
             user_input = int(input(var))
@@ -23,9 +24,18 @@ def validacao_input(var):
         except ValueError:
             print("Erro. Tenta denovo.")
 
-def checkscore(board, jogadores):
+
+def reset(): #reset aos valores da board
+    for i in range(len(board)):
+        for j in range(len(board)):
+            board[i][j] = '_'
+
+def checkscore(board, jogadores): #verificar se há combinação vencedora
     for i in range(len(board)):
         if all(cell == jogadores for cell in board[i]) or all(board[j][i] == jogadores for j in range(len(board))):
+            #all() retorna True caso os parametros sejam True
+            #primeio all() verifica cada linha da board, segundo all() verifica cada coluna
+            #cell itera sobre os valores de jogadores['X' ou 'O'] -> ex.: se
             return True
     if all(board[i][i] == jogadores for i in range(len(board))) or all(board[i][len(board) - 1 - i] == jogadores for i in range(len(board))):
         return True
@@ -33,7 +43,8 @@ def checkscore(board, jogadores):
 
 board = []
 jogadores = ['X','O']
-turn = 0
+ronda = 1
+roundc = 0
 makeboard()
 jogador = 0
 print(f"""
@@ -41,12 +52,28 @@ print(f"""
     """)
 while True:
     showboard()
-    print(f"Jogador {jogadores[jogador]}")
     jogadax = validacao_input("Coordenada X: ")
     jogaday = validacao_input("Coordenada Y: ")
+    while board[jogaday - 1][jogadax - 1] == 'X' or board[jogaday - 1][jogadax - 1] == 'O':
+        print("Coordenada ocupada. Tenta denovo.")
+        jogadax = validacao_input("Coordenada X: ")
+        jogaday = validacao_input("Coordenada Y: ")
     board[jogaday-1][jogadax-1] = jogadores[jogador]
     if checkscore(board, jogadores[jogador]):
         showboard()
-        print(f"Jogador {jogadores[jogador]} ganhou!")
-        break
+        resp = input(f"Jogador {jogadores[jogador]} ganhou!\n Repetir (s/n)? ")
+        if resp == 'n':
+            print("\nObrigado por jogares")
+            break
+        else:
+            reset()
+            ronda = 1
+            roundc = 0
+            continue
     jogador = (jogador + 1) % 2
+    if roundc == 1:
+        ronda += 1
+        roundc = 0
+    else:
+        roundc += 1
+    print(board)
