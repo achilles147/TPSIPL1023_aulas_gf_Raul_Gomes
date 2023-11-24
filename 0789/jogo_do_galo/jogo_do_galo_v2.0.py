a4743613776800f55597bfc361288aff7a011784
achilles147 <raul19david@gmail.com>
2023-11-24 01:58:51 +0000

    jogo do galo v2.0

--- 0789/jogo_do_galo/jogo_do_galo_v2.0.py
+++ 0789/jogo_do_galo/jogo_do_galo_v2.0.py
@@ -1,16 +1,48 @@
 #criar board n-elementos
-def board():
-    n_board = int(input("n: "))
-    board = []
+
+def makeboard():
+    n_board = int(input("N-elementos: "))
     for i in range(n_board):
         linha = []
         for j in range(n_board):
             linha.append('_')
         board.append(linha)
-    print(board)
+def showboard():
     for i in range(len(board)):
         for j in range(len(board)):
-            print(board[i][j], end="\t")
+            print(board[i][j], end="\t\t")
         print("\n")
 
-board()
+def checkscore(board):
+    for linha in board:
+        if all(value == linha[0] for value in linha):
+            return True
+
+
+
+
+board = []
+turn = 0
+makeboard()
+print(f"""
+{"="*5+"Jogo do Galo"+"="*5}
+    """)
+while True:
+    showboard()
+    if turn == 0:
+        print("Jogador X")
+        jogadax = int(input("Coordenada x: "))
+        jogaday = int(input("Coordenada Y: "))
+        board[jogaday-1][jogadax-1] = 'X'
+        turn = 1
+    elif turn == 1:
+        print("Jogador Y")
+        jogadax = int(input("Coordenada x: "))
+        jogaday = int(input("Coordenada Y: "))
+        board[jogaday - 1][jogadax - 1] = 'Y'
+        turn = 0
+    if checkscore(board):
+        print("Linha!")
+    else:
+        print("perdeu playboi")
+    print(board)
